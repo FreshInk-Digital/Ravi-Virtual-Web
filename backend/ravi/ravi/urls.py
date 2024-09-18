e26198073b8cb8e74bea1484b154294c4f12ad40
Jacobgijah <jacobgijjah@gmail.com>
2024-09-19 02:30:01 +0300

    update admin site title in ravi/urls.py file

--- backend/ravi/ravi/urls.py
+++ backend/ravi/ravi/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Customizing the Django Admin interface
+admin.site.site_header = "Ravi Moova Admin"
+admin.site.site_title = "Ravi Moova Admin Portal"
+admin.site.index_title = "Welcome to Ravi Moova Admin Dashboard"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('docs.urls')),
