6d8d9e78ac4aa2141ee47ed3eb37312762616572
Jacobgijah <jacobgijjah@gmail.com>
2024-09-19 02:21:00 +0300

    add cors allowed host point to frontend host of ravimoova

--- backend/ravi/ravi/settings.py
+++ backend/ravi/ravi/settings.py
@@ -65,8 +65,7 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
   'http://localhost:8000',
   'http://127.0.0.1:8000',
-  'https://ravimoova.co.tz:2083',
-  'https://ravimoova.co.tz:2083',
+  'https://ravimoova.co.tz',
 ]
 
 ROOT_URLCONF = 'ravi.urls'
